e74c292bc2cb1ea2dba2d5ecc3812521f3bc363e
Jacky Leung <bbnin7a@gmail.com>
2023-05-27 16:00:26 +0800

    feat: complete basic favorites page

---
+++ app/favorites/FavoritesClient.tsx
@@ -0,0 +1,31 @@
+import Container from "../components/Container";
+import Heading from "../components/Heading";
+import ListingCard from "../components/listings/ListingCard";
+import { SafeListing, SafeUser } from "../types";
+
+interface FavoritesClientProps {
+  listings: SafeListing[];
+  currentUser?: SafeUser | null;
+}
+
+const FavoritesClient: React.FC<FavoritesClientProps> = ({
+  listings,
+  currentUser,
+}) => {
+  return (
+    <Container>
+      <Heading title="Favorites" subtitle="List of places you have favorited" />
+      <div className="subcontainer">
+        {listings.map((listing) => (
+          <ListingCard
+            currentUser={currentUser}
+            key={listing.id}
+            data={listing}
+          />
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default FavoritesClient;
